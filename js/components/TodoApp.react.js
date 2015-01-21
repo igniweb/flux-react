1a744e9e2e19cce17ea7f54b7f7220abce43530a
Sébastien Muller <igniweb.net@gmail.com>
2015-01-22 00:18:42 +0100

    Init todo app

---
+++ js/components/TodoApp.react.js
@@ -0,0 +1,18 @@
+var TodosStore = require('../stores/TodosStore');
+
+function getTodoState() {
+    return {
+        allTodos: TodosStore.getAll(),
+        areAllComplete: TodosStore.areAllComplete()
+    };
+}
+
+var TodoApp = React.createClass({
+
+    getInitialState: function () {
+        return getTodoState();
+    },
+
+
+
+});
